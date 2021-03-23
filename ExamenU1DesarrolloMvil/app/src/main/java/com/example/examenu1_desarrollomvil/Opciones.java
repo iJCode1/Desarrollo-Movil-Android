f832c3bff013d788d8d03a92dcbde4cd92fd11d8
Joel <joeldome17@gmail.com>
2021-03-22 18:53:20 -0600

    Añadiendo Reloj

--- ExamenU1DesarrolloMvil/app/src/main/java/com/example/examenu1_desarrollomvil/Opciones.java
+++ ExamenU1DesarrolloMvil/app/src/main/java/com/example/examenu1_desarrollomvil/Opciones.java
@@ -2,11 +2,18 @@ package com.example.examenu1_desarrollomvil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class Opciones extends AppCompatActivity {
 
@@ -40,7 +47,8 @@ public class Opciones extends AppCompatActivity {
     }
 
     public void reloj(View view){
-
+        Intent i = new Intent(this, Reloj.class);
+        startActivity(i);
     }
 
     public void diferente(View view){
@@ -63,4 +71,7 @@ public class Opciones extends AppCompatActivity {
     public void salir(View view){
         finishAffinity();
     }
+
+
+
 }
---
+++ ExamenU1DesarrolloMvil/app/src/main/java/com/example/examenu1_desarrollomvil/Reloj.java
@@ -0,0 +1,61 @@
+package com.example.examenu1_desarrollomvil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class Reloj extends AppCompatActivity {
+
+    TextView tv1, tv2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reloj);
+
+        tv1 = (TextView)findViewById(R.id.txtFecha);
+        tv2 = (TextView)findViewById(R.id.txtHora);
+
+        String fecha = obtenerFechaActual("America/Mexico_City");
+        tv1.setText(fecha);
+
+        String hora = obtenerHoraActual("America/Mexico_City");
+        tv2.setText(hora);
+    }
+
+
+    @SuppressLint("SimpleDateFormat")
+    public static String obtenerFechaConFormato(String formato, String zonaHoraria) {
+        Calendar calendar = Calendar.getInstance();
+        Date date = calendar.getTime();
+        SimpleDateFormat sdf;
+        sdf = new SimpleDateFormat(formato);
+        sdf.setTimeZone(TimeZone.getTimeZone(zonaHoraria));
+        return sdf.format(date);
+    }
+
+    public static String obtenerFechaActual(String zonaHoraria) {
+        String formato = "yyyy-MM-dd";
+        return obtenerFechaConFormato(formato, zonaHoraria);
+    }
+
+    public static String obtenerHoraActual(String zonaHoraria) {
+        String formato = "HH:mm:ss";
+        return obtenerFechaConFormato(formato, zonaHoraria);
+    }
+
+    public void regresar(View view){
+        Intent i = new Intent(this, Opciones.class);
+        startActivity(i);
+    }
+}
