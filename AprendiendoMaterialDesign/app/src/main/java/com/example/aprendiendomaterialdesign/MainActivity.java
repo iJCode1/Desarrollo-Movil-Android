3463770e4d2c4996bce67b8a614d2c38f54f1ec6
Joel <joeldome17@gmail.com>
2021-03-26 19:20:38 -0600

    Añadiendo botón flotante

--- AprendiendoMaterialDesign/app/src/main/java/com/example/aprendiendomaterialdesign/MainActivity.java
+++ AprendiendoMaterialDesign/app/src/main/java/com/example/aprendiendomaterialdesign/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.aprendiendomaterialdesign;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Salir(View view){
+        finishAffinity();
+    }
+
+    public void Gato(View view){
+        Toast.makeText(this, "Miauuuuuuuuu", Toast.LENGTH_LONG).show();
     }
 }
