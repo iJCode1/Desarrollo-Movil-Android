ff66767a5ea398fbf96fa455df32d3f1abf3110e
Joel <joeldome17@gmail.com>
2021-03-26 19:56:02 -0600

    Añadiendo segundo activity a material desing

--- AprendiendoMaterialDesign/app/src/main/java/com/example/aprendiendomaterialdesign/MainActivity.java
+++ AprendiendoMaterialDesign/app/src/main/java/com/example/aprendiendomaterialdesign/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.aprendiendomaterialdesign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
@@ -20,5 +21,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void Gato(View view){
         Toast.makeText(this, "Miauuuuuuuuu", Toast.LENGTH_LONG).show();
+    }
+
+    public void segundoActivity(View view){
+        Intent i = new Intent(this, activity2.class);
+        startActivity(i);
     }
 }
