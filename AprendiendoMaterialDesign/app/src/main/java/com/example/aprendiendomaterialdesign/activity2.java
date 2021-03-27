bf15d339f09b74008b3ace90a3956fb6f819e96e
Joel <joeldome17@gmail.com>
2021-03-27 08:25:41 -0600

    Aplicación Finalizada Material Design

--- AprendiendoMaterialDesign/app/src/main/java/com/example/aprendiendomaterialdesign/activity2.java
+++ AprendiendoMaterialDesign/app/src/main/java/com/example/aprendiendomaterialdesign/activity2.java
@@ -3,6 +3,7 @@ package com.example.aprendiendomaterialdesign;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class activity2 extends AppCompatActivity {
 
@@ -10,6 +11,10 @@ public class activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity2);
+    }
+
+    public void Salir(View view){
+        finishAffinity();
     }
 }
 
