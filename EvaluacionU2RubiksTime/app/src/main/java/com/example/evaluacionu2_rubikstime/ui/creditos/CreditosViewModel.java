2dc3763179834f0593428ec8cf22c8940fbf847c
Joel <joeldome17@gmail.com>
2021-05-03 10:46:20 -0500

    Fragment de Creditos añadido

---
+++ EvaluacionU2RubiksTime/app/src/main/java/com/example/evaluacionu2_rubikstime/ui/creditos/CreditosViewModel.java
@@ -0,0 +1,7 @@
+package com.example.evaluacionu2_rubikstime.ui.creditos;
+
+import androidx.lifecycle.ViewModel;
+
+public class CreditosViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
