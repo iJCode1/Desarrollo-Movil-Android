1b694700a25a0b0bef18bd3d44c9424e2ffec4c3
Joel <joeldome17@gmail.com>
2021-05-03 15:32:30 -0500

    Añadiendo Interfaz de Altas

--- EvaluacionU2RubiksTime/app/src/main/java/com/example/evaluacionu2_rubikstime/ui/altas/AltasFragment.java
+++ EvaluacionU2RubiksTime/app/src/main/java/com/example/evaluacionu2_rubikstime/ui/altas/AltasFragment.java
@@ -11,28 +11,43 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.evaluacionu2_rubikstime.R;
 
 public class AltasFragment extends Fragment {
 
+
+    private Spinner sCategoria, sStatus;
+
+    private View root;
+
+    private ArrayAdapter<CharSequence> CatAdapter, StatusAdapter;
+
     private AltasViewModel mViewModel;
 
-    public static AltasFragment newInstance() {
-        return new AltasFragment();
-    }
+
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_altas, container, false);
+        root = inflater.inflate(R.layout.fragment_altas, container, false);
+        initComponents();
+        return root;
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(AltasViewModel.class);
-        // TODO: Use the ViewModel
+    private void initComponents() {
+
+        sCategoria = root.findViewById(R.id.fa_cubos_categoria);
+        sStatus = root.findViewById(R.id.fa_status);
+
+        CatAdapter = ArrayAdapter.createFromResource(getContext(),R.array.cubes_category,android.R.layout.simple_spinner_item);
+        StatusAdapter = ArrayAdapter.createFromResource(getContext(),R.array.status,android.R.layout.simple_spinner_item);
+
+        sCategoria.setAdapter(CatAdapter);
+        sStatus.setAdapter(StatusAdapter);
     }
+
 
 }
