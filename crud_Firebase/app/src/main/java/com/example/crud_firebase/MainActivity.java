a39172e8ecdbd42c50c4babf5a7f0b452f5896cb
Joel <joeldome17@gmail.com>
2021-05-19 08:03:17 -0500

    Añadiendo Diseño del Crud y Menú

---
+++ crud_Firebase/app/src/main/java/com/example/crud_firebase/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.crud_firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.example.crud_firebase.dbfb.Persona;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+
+
+}
---
+++ crud_Firebase/app/src/main/java/com/example/crud_firebase/dbfb/Persona.java
@@ -0,0 +1,55 @@
+package com.example.crud_firebase.dbfb;
+
+public class Persona {
+    private String Uid;
+    private String Nombre;
+    private String Apellido;
+    private String Correo;
+    private String Password;
+
+    public Persona(){}
+
+    public String getUid() {
+        return Uid;
+    }
+
+    public void setUid(String uid) {
+        Uid = uid;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getApellido() {
+        return Apellido;
+    }
+
+    public void setApellido(String apellido) {
+        Apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String correo) {
+        Correo = correo;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String toString(){
+        return (Nombre + " " + Apellido);
+    }
+}
