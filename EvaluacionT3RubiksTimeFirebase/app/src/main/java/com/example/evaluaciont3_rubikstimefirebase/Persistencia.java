58de6808b9b2a369d2756e9ea06c2401b6ce56f3
Joel <joeldome17@gmail.com>
2021-05-29 15:45:58 -0500

    Cambiando icono, colores y agregando persistencia de datos

---
+++ EvaluacionT3RubiksTimeFirebase/app/src/main/java/com/example/evaluaciont3_rubikstimefirebase/Persistencia.java
@@ -0,0 +1,13 @@
+package com.example.evaluaciont3_rubikstimefirebase;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Persistencia extends android.app.Application{
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+
+        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+    }
+}
